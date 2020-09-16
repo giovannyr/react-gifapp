94771b123c82b9b9eb3626ac0520f96af88479f2
Arturo <arturo.9019@gmail.com>
2020-09-16 12:20:43 -0500

    Docs agregado

---
+++ docs/precache-manifest.4087150a4fc5de0a776218e66aa15d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cdacc20a9dcab0062559f459df1f7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2094b5e98cb75e18191a",
+    "url": "/static/css/main.ff5fb51f.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2094b5e98cb75e18191a",
+    "url": "/static/js/main.da7aa046.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
